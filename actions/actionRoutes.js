77018a0092dc90114fbdc2e8b385e6f457f60e57
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-05-19 21:30:56 -0400

    Get request to / configured for both projects and actions

---
+++ actions/actionRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+const db = require("../data/helpers/actionModel");
+
+router.get("/", async (req, res) => {
+  try {
+    const data = await db.get();
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error!" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const projects = require("./projects/projectRoute");
+const actions = require("./actions/actionRoutes");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use("/api/projects/", projects);
+server.use("/api/actions/", actions);
 
 module.exports = server;
