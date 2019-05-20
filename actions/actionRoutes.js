c3ea1d0ca2b5e1070d4c9e49eab03c3a8cbaf15d
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-05-19 22:15:47 -0400

    Crud Operations completed for projects

--- actions/actionRoutes.js
+++ actions/actionRoutes.js
@@ -13,4 +13,29 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await db.get(req.params.id);
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error!" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { description, notes, completed } = req.body;
+  const action = {
+    project_id: 1,
+    description: description,
+    notes: notes,
+    completed: completed
+  };
+  try {
+    const data = await db.insert(action);
+    res.status(201).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error!" });
+  }
+});
+
 module.exports = router;
--- projects/projectRoute.js
+++ projects/projectRoute.js
@@ -12,5 +12,47 @@ router.get("/", async (req, res) => {
     res.status(500).json({ err, message: "Internal Server Error!" });
   }
 });
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await db.get(req.params.id);
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error!" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, description, completed } = req.body;
+  const project = {
+    name: name,
+    description: description,
+    completed: completed
+  };
+  try {
+    const data = await db.insert(project);
+    res.status(201).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error!" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = await db.remove(req.params.id);
+    res.status(204).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error!" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const changes = req.body;
+  try {
+    const data = await db.update(req.params.id, changes);
+    res.status(202).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error!" });
+  }
+});
 
 module.exports = router;
